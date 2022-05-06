ae7787b68979d64ac9b15e19c227d38c6c61bdcc
tdnIsma <ismaelbraun5@gmail.com>
2022-05-05 21:36:23 -0300

    aula05-05-2022

---
+++ site-05-05-2022/tds03.php
@@ -0,0 +1,19 @@
+<?php
+
+    $var1 = 5;
+    $var2 = 10.5;
+    $var3 = "Curso senac";
+    $soma = $var1 + $var2;
+    $soma2 = $var1 + $var3;
+
+    echo $var1;
+    echo "<br>";
+    echo $var2;
+    echo "<br>";
+    echo $var3;
+    echo "<br>";
+    echo $soma;
+    echo "<br>";
+    echo $soma2;
+
+?>
